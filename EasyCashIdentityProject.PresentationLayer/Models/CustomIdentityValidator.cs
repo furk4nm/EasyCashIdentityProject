89e1d4bcf60e4fdccce10117ae962a4f3720a060
furkan <furka@DESKTOP-TRR628R>
2023-05-19 19:30:23 +0300

    Register Validatior türkçe olarak eklendi

---
+++ EasyCashIdentityProject.PresentationLayer/Models/CustomIdentityValidator.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace EasyCashIdentityProject.PresentationLayer.Models
+{
+	public class CustomIdentityValidator:IdentityErrorDescriber
+	{
+		public override IdentityError PasswordTooShort(int length)
+		{
+			return new IdentityError()
+			{
+				Code = "PasswordToShort",
+				Description = $"Parola en az {length} Karakter olmalıdır"
+			};
+		}
+		public override IdentityError PasswordRequiresUpper()
+		{
+			return new IdentityError()
+			{
+				Code = "PasswordRequires",
+				Description = "Lütfen en az 1 büyük harf giriniz"
+			};
+		}
+
+		public override IdentityError PasswordRequiresLower() 
+		{
+			return new IdentityError()
+			{
+				Code = "PasswordRequiresLower",
+				Description = "Lütfen en az 1 küçük harf giriniz"
+			};
+
+		}
+		public override IdentityError PasswordRequiresDigit() 
+		{
+			return new IdentityError()
+			{
+				Code = "PasswordRequiresDigit",
+				Description = "Lütfen en az 1 tane rakam giriniz"
+			};
+
+		}
+		public override IdentityError PasswordRequiresNonAlphanumeric()
+		{
+			return new IdentityError()
+			{
+				Code = "PasswordRequiresNonAlphanumeric",
+				Description = "Lütfen en az 1 tane sembol giriniz"
+			};
+			
+		}
+
+
+	}
+}
